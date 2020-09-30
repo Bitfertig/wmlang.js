d7bec844e5fdbb24ce9648c2dfb6d257b9d36f51
Aurelian Hermand <aurel@hermand.de>
2020-09-30 11:45:34 +0200

    package.json fix and documentation changes

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 http://tools.bitfertig.de/wmlang.js/
 
+
 ## Install
 
 ```bash
 npm i @dipser/wmlang.js
 ```
+
 
 ## Usage
 
@@ -19,3 +21,19 @@ console.log( weekday(1, 'de') ); // => Montag
 console.log( month(1, 'de') ); // => Januar
 </script>
 ```
+
+
+## How it works
+
+```javascript
+function month(month, locale = 'en') {
+    let intl = new Intl.DateTimeFormat(locale, { month: 'long' });
+    return intl.format( new Date(1970, Number(month) - 1, 1) );
+}
+month(2, 'de'); // => Februar
+```
+
+
+## More
+* [npm package](https://www.npmjs.com/package/@dipser/wmlang.js)
+* [twitter](https://twitter.com/dipser)
--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 /**
- * Translation of a month.
- * @param {*} month 1 - 12
- * @param {*} locale en, de, ...
- */
-function month(month, locale = 'en') {
-    let intl = new Intl.DateTimeFormat(locale, { month: 'long' });
-    return intl.format( new Date(1970, Number(month) - 1, 1) );
-}
-
-/**
  * Translation of a weekday.
  * @param {*} weekday 1 - 7
  * @param {*} locale  en, de, ...
@@ -16,6 +6,16 @@ function month(month, locale = 'en') {
 function weekday(weekday, locale = 'en') {
     let intl = new Intl.DateTimeFormat(locale, { weekday: 'long' });
     return intl.format( new Date(1970, 0, Number(weekday) + 4 ) );
+}
+
+/**
+ * Translation of a month.
+ * @param {*} month 1 - 12
+ * @param {*} locale en, de, ...
+ */
+function month(month, locale = 'en') {
+    let intl = new Intl.DateTimeFormat(locale, { month: 'long' });
+    return intl.format( new Date(1970, Number(month) - 1, 1) );
 }
 
 export { weekday, month };
